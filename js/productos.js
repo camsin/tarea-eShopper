c841fda22e33d0932a41abc9b298b70b46e39f5f
Ararat Mendez <ararat.mendez@gmail.com>
2018-02-20 17:49:58 -0700

    first commit

---
+++ js/productos.js
@@ -0,0 +1,5 @@
+
+var prods = [
+	{"nombre":"Papas", "precio":10.0, "img":"https://super.walmart.com.mx/images/product-images/img_large/00750101113388L.jpg"},
+	{"nombre":"Donas", "precio":15.0, "img":"https://super.walmart.com.mx/images/product-images/img_large/00750100011242L.jpg"}
+]
